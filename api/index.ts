2bccd9e6c589b25a7f9e45e6cd442e5229b3d2b8
Dengju Deng <wj871287@gmail.com>
2021-05-24 21:28:40 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -96,7 +96,7 @@ export const generateSVG = async ({ text, url }) => {
         </g>
       </g>
 
-      <g transform="translate(260, 18)" >
+      <g transform="translate(230, 18)" >
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="64" height="64">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" x="0" y="0">
             <a href="${url}" target="_blank">
